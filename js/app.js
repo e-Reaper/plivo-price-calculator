9a1494db821b3b572180a707ba752bd89131e265
alk <abhishek.anand@igp.com>
2016-09-13 04:40:01 +0530

    added responsiveness

---
+++ js/app.js
@@ -0,0 +1,74 @@
+var plivoApp = function () {
+	this.country = pricingData[0].country;
+	this.usage = null;
+	this.pricing = pricingData[0];
+	this.loadCountries = function() {
+		var html = '';
+		for (var i = 0; i < pricingData.length; i++) {
+			html += '<option value="' + pricingData[i].country + '">' + pricingData[i].country_name + '</option>';
+		}
+		document.getElementById('country-data').innerHTML = html;
+	};
+
+	this.setUsage = function(element) {
+		var self = this;
+		self.usage = document.getElementById(element.id).value;	
+		var twilio;
+		var plivo;
+		var saving;
+		if (self.usage >= 500) {
+			twilio = parseInt(self.pricing.twilio * self.usage);
+			plivo = parseInt(self.pricing.plivo * self.usage);
+			saving = ((twilio - plivo) * 12);
+		}else{
+			twilio = parseFloat(self.pricing.twilio * self.usage).toFixed(2);
+			plivo = parseFloat(self.pricing.plivo * self.usage).toFixed(2);	
+			saving = ((twilio - plivo) * 12).toFixed(2);
+		}
+		var html = '<div class="col">$' + plivo + '</div><div class="col">$' + twilio + '</div><div class="col">$'+ saving +'</div>';
+		document.getElementById('pricing-data').innerHTML = html;
+	}
+
+	this.setCountry = function(element) {
+		var self = this;
+		self.country = document.getElementById(element.id).value;
+		for (var i = 0; i < pricingData.length; i++) {
+			if (pricingData[i].country === self.country){
+				self.pricing = pricingData[i];
+			}
+		}
+	}
+}
+
+window.onload = function (argument) {
+	var app = new plivoApp();
+	app.loadCountries();
+	document.getElementById('country-data').addEventListener('change',function(e) {
+		document.getElementById('usage-data').value = '';
+		app.setCountry(this);
+		showLoader();	
+	});
+	document.getElementById('usage-data').addEventListener('keyup',function(e) {
+		this.value = this.value.replace(/[a-zA-z]/g, "");
+		if ((e.keyCode <= 57 && e.keyCode >= 48) || e.keyCode == 8 ) {
+			console.log(this.value);
+			if (this.value > 0){
+				app.setUsage(this);
+				hideLoader();
+			}else{
+				showLoader();
+			}
+
+		}else{
+
+		}
+	});
+}
+
+function hideLoader() {
+	document.getElementById('loading-message').className = 'loading-message hidden';
+}
+
+function showLoader() {
+	document.getElementById('loading-message').className = 'loading-message shown';
+}
---
+++ js/data.js
@@ -0,0 +1,86 @@
+var pricingData = [
+	{
+		country : 'IN',
+		country_name : 'India',
+		plivo : 0.00175,
+		twilio : 0.0075
+	},
+	{
+		country : 'US',
+		country_name : 'United States',
+		plivo : 0.00179,
+		twilio : 0.0079
+	},
+	{
+		country : 'GB',
+		country_name : 'United Kingdom',
+		plivo : 0.00180,
+		twilio : 0.0080
+	},
+	{
+		country : 'AU',
+		country_name : 'Australia',
+		plivo : 0.00185,
+		twilio : 0.0090
+	},
+	{
+		country : 'CO',
+		country_name : 'Colombia',
+		plivo : 0.00195,
+		twilio : 0.0075
+	},
+	{
+		country : 'FR',
+		country_name : 'France',
+		plivo : 0.00195,
+		twilio : 0.0095
+	},
+	{
+		country : 'GR',
+		country_name : 'Greece',
+		plivo : 0.00205,
+		twilio : 0.0085
+	},
+	{
+		country : 'ID',
+		country_name : 'Indonesia',
+		plivo : 0.00175,
+		twilio : 0.0095
+	},
+	{
+		country : 'JP',
+		country_name : 'Japan',
+		plivo : 0.00275,
+		twilio : 0.0105
+	},
+	{
+		country : 'MX',
+		country_name : 'Mexico',
+		plivo : 0.00175,
+		twilio : 0.0095
+	},
+	{
+		country : 'UA',
+		country_name : 'Ukraine',
+		plivo : 0.00165,
+		twilio : 0.0055
+	},
+	{
+		country : 'AE',
+		country_name : 'United Arab Emirates',
+		plivo : 0.00275,
+		twilio : 0.0060
+	},
+	{
+		country : 'VN',
+		country_name : 'Viet Nam',
+		plivo : 0.00235,
+		twilio : 0.0060
+	},
+	{
+		country : 'TR',
+		country_name : 'Turkey',
+		plivo : 0.00265,
+		twilio : 0.0095
+	}	
+];
